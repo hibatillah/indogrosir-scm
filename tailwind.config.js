8f73cc938a22534da409866c0ea4d6de552399b5
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-11-23 17:49:42 +0700

    setup & add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        './Views/**/*.cshtml'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                foreground: '#F6F8FA'
+            }
+        },
+    },
+    plugins: [],
+}
