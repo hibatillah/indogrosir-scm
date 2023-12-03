3362d402776f61c617a773850a6e239ede80aeab
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-12-03 17:12:40 +0700

    refactor: update active page menu add mitra controller

---
+++ Controllers/MitraController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace indogrosir_tim8.Controllers
+{
+    public class MitraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profil()
+        {
+            return View();
+        }
+
+        public IActionResult Produk()
+        {
+            return View();
+        }
+
+        public IActionResult Pesanan()
+        {
+            return View();
+        }
+    }
+}
